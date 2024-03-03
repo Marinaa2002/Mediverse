1ddf64e19ed33480bf44bfae03d87ad6e0536b8a
Marina Milad Tadros <marina.milad2002@gmail.com>
2024-03-03 16:13:02 +0200

    Create Lab Results Cubit

---
+++ mediverse/lib/Features/PatientDashboard/MedicalRecord/LabResultsScreen/presentation/Manager/lab_result_cubit/lab_result_cubit.dart
@@ -0,0 +1,43 @@
+import 'package:bloc/bloc.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:mediverse/Features/PatientDashboard/MedicalRecord/LabResultsScreen/data/models/labResult_model.dart';
+import 'package:meta/meta.dart';
+
+import '../../../data/repos/labResult_repo.dart';
+
+
+part 'lab_result_state.dart';
+
+class LabResultCubit extends Cubit<LabResultState> {
+  LabResultCubit(this.labResultsRepo ) : super(LabResultInitial());
+
+  final LabResultsRepo labResultsRepo;
+  CollectionReference messages =
+  FirebaseFirestore.instance.collection('labs');
+  // List<QueryDocumentSnapshot> data;
+  // int index;
+
+  void sendLabModel({required String now_date, required String imageUrl}) {
+    emit(LabResultLoading());
+    try {
+      labResultsRepo.sendLabs(now_date: now_date, imageUrl: imageUrl);
+    } on Exception catch (e) {
+      emit(LabResultFailure('Something went wrong, Try again'));
+    }
+  }
+
+  void getLabModels() async{
+    emit(LabResultLoading());
+    await messages.orderBy('createdAt', descending: true).snapshots().listen((event) async {
+      var result = await labResultsRepo.getLabs(event: event);
+      result.fold((left) =>
+          emit(LabResultFailure(left.errMsg)),
+              (right) =>
+                  emit(LabResultSuccess(labModelList: right))
+      );
+    });
+  }
+
+}
