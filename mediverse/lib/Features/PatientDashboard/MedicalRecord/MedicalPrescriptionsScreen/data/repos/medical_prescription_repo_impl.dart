dc6e58cec0fc3f19058a422212c52df8a65bd7e5
Marina Milad Tadros <marina.milad2002@gmail.com>
2024-03-17 17:15:38 +0200

    Create Medical Prescription Repo Implementation

---
+++ mediverse/lib/Features/PatientDashboard/MedicalRecord/MedicalPrescriptionsScreen/data/repos/medical_prescription_repo_impl.dart
@@ -0,0 +1,38 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dartz/dartz.dart';
+import 'package:mediverse/Features/PatientDashboard/MedicalRecord/MedicalPrescriptionsScreen/data/models/medical_prescription_model.dart';
+import 'package:mediverse/Features/PatientDashboard/MedicalRecord/MedicalPrescriptionsScreen/data/repos/medical_prescription_repo.dart';
+
+import '../../../../../../Core/Errors/Failure.dart';
+
+class MedicalPrescriptionRepoImpl extends MedicalPrescriptionRepo{
+  CollectionReference messages =
+  FirebaseFirestore.instance.collection('medicalPrescriptions');
+
+  @override
+  sendLabs({required String now_date, required String imageUrl}) {
+    try {
+      messages.add({
+        'createdAt': DateTime.now(),
+        'pictureDate': now_date,
+        'imageUrl' : imageUrl,
+      });
+    } on Exception catch (e) {
+
+    }
+  }
+
+  @override
+  Future<Either<Failure , List<MedicalPrescriptionModel>>> getLabs({required QuerySnapshot event}) async{
+    try {
+      List<MedicalPrescriptionModel> messageList = [];
+      for (var doc in event.docs) {
+        messageList.add(MedicalPrescriptionModel.fromJson(doc));
+      }
+      return right(messageList);
+    } on Exception catch (e) {
+      return left(ServerFailure(errMsg: 'Something went wrong, Try again'));
+    }
+  }
+
+}
