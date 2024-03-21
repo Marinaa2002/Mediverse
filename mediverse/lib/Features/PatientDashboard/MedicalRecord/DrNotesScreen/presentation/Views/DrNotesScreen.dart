3c58c4b2c85145fb51d308470863f99b33d5185e
Philopateer Moheb <philopateermoheb1901202@gmail.com>
2024-03-21 12:21:59 +0200

    Feat:Edit View Notes App

--- mediverse/lib/Features/PatientDashboard/MedicalRecord/DrNotesScreen/presentation/Views/DrNotesScreen.dart
+++ mediverse/lib/Features/PatientDashboard/MedicalRecord/DrNotesScreen/presentation/Views/DrNotesScreen.dart
@@ -1,67 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:mediverse/Constants/constant.dart';
-import 'package:mediverse/Features/PatientDashboard/Widgets/CustomAdviceWidget.dart';
-import 'package:mediverse/Features/PatientDashboard/Widgets/CustomDateWidget.dart';
-import 'package:mediverse/Features/PatientDashboard/Widgets/SendAdviceBox.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class DrNotesScreen extends StatelessWidget {
-  DrNotesScreen({super.key});
-
-  final scaffoldKey = GlobalKey<ScaffoldState>();
+class NotesView extends StatelessWidget {
+  const NotesView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: scaffoldKey,
-      backgroundColor: backgroundColor,
-      appBar: AppBar(
-        backgroundColor: kprimaryColor,
-        automaticallyImplyLeading: false,
-        leading: IconButton(
-          icon: const Icon(
-            Icons.chevron_left,
-            color: kSecondryBackgroundColor,
-            size: 24,
-          ),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        ),
-        title: const Text('My Medical Notes',
-            style: TextStyle(
-              fontFamily: 'Outfit',
-              color: Colors.white,
-              fontSize: 20,
-            )),
-        actions: [],
-        centerTitle: true,
-        elevation: 2,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(
-              child: ListView(
-                children: [
-                  CustomDateWidget(date: '8/8/2023'),
-                  CustomAdviceWidget(
-                      advice: ' Don\'t eat fatty foods for you cholestrol'),
-                  CustomDateWidget(date: '15/9/2023'),
-                  CustomAdviceWidget(
-                      advice: ' Drink about 2 litres of water per day'),
-                  CustomDateWidget(date: '20/9/2023'),
-                  CustomAdviceWidget(
-                      advice:
-                          'Make sure to eat well before taking your medication'),
-                ],
+      resizeToAvoidBottomInset: false,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+              isScrollControlled: true,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
               ),
-            ),
-            const SendAdviceBox(),
-          ],
-        ),
+              context: context,
+              builder: (context) {
+                return const AddNoteBottomSheet();
+              });
+        },
+        child: const Icon(Icons.add),
       ),
+      body: const NotesViewBody(),
     );
   }
 }
