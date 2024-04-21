8d15fb65b246af75c1d26cf136bff37fbe8b873d
Marina Milad Tadros <marina.milad2002@gmail.com>
2024-04-21 20:59:25 +0200

    Update Patient Profile Repo

--- mediverse/lib/Features/PatientDashboard/PatientProfileScreen/data/repos/ProfilePatientRepo.dart
+++ mediverse/lib/Features/PatientDashboard/PatientProfileScreen/data/repos/ProfilePatientRepo.dart
@@ -1,9 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:either_dart/either.dart';
+import 'package:dartz/dartz.dart';
 import 'package:mediverse/Features/PatientDashboard/PatientProfileScreen/data/models/PatientProfileModel.dart';
 import '../../../../../Core/Errors/Failure.dart';
 
 abstract class ProfilePatientRepo{
-  updatePatientInfo({required String? uid,required String name, required String age, required String phoneNum, required String email, required String national_id});
-  Future<Either<Failure , List<PatientProfileModel>>> getPatientInfo({required QuerySnapshot event});
+  updateName({required String? name});
+  updateAge({required String? age});
+  updateNationalId({required String? nationalId});
+  updatePhoneNum({required String? phoneNum});
+  updatePicture({required String? picture});
 }
