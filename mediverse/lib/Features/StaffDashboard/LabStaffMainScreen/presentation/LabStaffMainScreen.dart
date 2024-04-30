de65f5a4048e57aaa971ce6d7815a23c8a8bc413
Philopateer Moheb <philopateermoheb1901202@gmail.com>
2024-04-30 23:40:43 +0300

    Feat:Add Alert Dialog to lab result

--- mediverse/lib/Features/StaffDashboard/LabStaffMainScreen/presentation/LabStaffMainScreen.dart
+++ mediverse/lib/Features/StaffDashboard/LabStaffMainScreen/presentation/LabStaffMainScreen.dart
@@ -1,14 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mediverse/Constants/Themes.dart';
 import 'package:mediverse/Constants/constant.dart';
+import 'package:mediverse/Core/utils/Functions.dart';
+import 'package:mediverse/Core/utils/Globals.dart';
 import 'package:mediverse/Features/StaffDashboard/HospitalStaffManagementScreenAddDoctors/presentation/Manager/FetechHospitalMangementCubit/fetech_HM_info_cubit.dart';
 import 'package:mediverse/Features/StaffDashboard/Widgets/ListViewPatient.dart';
 
 class LabStaffScreen extends StatelessWidget {
   LabStaffScreen({super.key});
   String id = '';
+  final TextEditingController textEditingController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     Map<String, dynamic>? args =
@@ -51,6 +56,38 @@ class LabStaffScreen extends StatelessWidget {
               const Expanded(
                 child: ListViewPatient(),
               ),
+              Padding(
+                padding: const EdgeInsets.only(right: 10.0, bottom: 20),
+                child: Align(
+                  alignment: Alignment.bottomRight,
+                  child: FloatingActionButton(
+                    child: Icon(
+                      Icons.add,
+                    ),
+                    onPressed: () async {
+                      final national_id = await showTextFieldDialog(context,
+                          textEditingController: textEditingController,
+                          title: "Add Patient 's national id",
+                          hintText: 'National id');
+                      if (national_id != null) {
+                        try {
+                          // Update the array in Firestore using arrayUnion
+                          await FirebaseFirestore.instance
+                              .collection('Staff')
+                              .doc(globalcurrentUserId)
+                              .update({
+                            'Jobs': FieldValue.arrayUnion([national_id]),
+                          });
+                          print('National ID added to the array successfully');
+                        } catch (error) {
+                          print(
+                              'Error adding National ID to the array: $error');
+                        }
+                      }
+                    },
+                  ),
+                ),
+              )
             ],
           ),
         ),
--- mediverse/lib/Features/StaffDashboard/Widgets/HospitalAddHideWidget.dart
+++ mediverse/lib/Features/StaffDashboard/Widgets/HospitalAddHideWidget.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mediverse/Constants/constant.dart';
+import 'package:mediverse/Core/utils/Globals.dart';
 import 'package:mediverse/Features/StaffDashboard/AdminMainScreen/presentation/Manager/AddHideCubit/AddHideCubit.dart';
 import 'package:mediverse/Features/StaffDashboard/AdminMainScreen/presentation/Manager/AddHideCubit/AddHideState.dart';
 
@@ -85,6 +86,29 @@ class SwitchWidget extends StatefulWidget {
 
 class _SwitchWidgetState extends State<SwitchWidget> {
   bool option = true;
+  @override
+  void initState() {
+    super.initState();
+    fetchData(); // Call the helper method to fetch data
+  }
+
+  Future<void> fetchData() async {
+    try {
+      DocumentSnapshot documentSnapshot = await FirebaseFirestore.instance
+          .collection('Staff')
+          .doc(widget.staffid)
+          .get();
+      var condition = documentSnapshot['Condition'];
+
+      // Update the option variable based on the condition
+      setState(() {
+        option = (condition == 'Show');
+      });
+    } catch (error) {
+      // Handle any errors that occur during data fetching
+      print('Error fetching data: $error');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
