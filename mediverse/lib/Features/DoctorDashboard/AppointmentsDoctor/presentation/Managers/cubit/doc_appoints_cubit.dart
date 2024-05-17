c5b948934d182d380f50e14cc4bf19b266df37b8
marcosherif <68437948+marcosherif@users.noreply.github.com>
2024-05-17 19:54:52 +0300

    sort bookings according to day

--- mediverse/lib/Features/DoctorDashboard/AppointmentsDoctor/presentation/Managers/cubit/doc_appoints_cubit.dart
+++ mediverse/lib/Features/DoctorDashboard/AppointmentsDoctor/presentation/Managers/cubit/doc_appoints_cubit.dart
@@ -11,12 +11,26 @@ class DocAppointsCubit extends Cubit<DocAppointsState> {
 
   final DocAppointsRepoImpl docAppointsRepoImpl;
 
+  // custom order for weekdays
+  final Map<String, int> weekdayOrder = {
+    'Saturday': 0,
+    'Sunday': 1,
+    'Monday': 2,
+    'Tuesday': 3,
+    'Wednesday': 4,
+    'Thursday': 5,
+    'Friday': 6,
+  };
+
   void getBookingsList(String doctor_id) async {
     emit(DocAppointsLoading());
     var bookings_result = await docAppointsRepoImpl.getBookings(doctor_id);
     bookings_result.fold(
       (left) => emit(DocAppointsFailed()),
       (right) async {
+        // Sort the list using the custom order
+        right.sort(
+            (a, b) => weekdayOrder[a.Day]!.compareTo(weekdayOrder[b.Day]!));
         var names_result = await docAppointsRepoImpl.getPatientNames(right);
         names_result.fold(
           (l) => emit(DocAppointsFailed()),
